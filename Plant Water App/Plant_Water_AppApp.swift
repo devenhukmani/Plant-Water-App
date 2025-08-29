2919b58e22505b205ecd67dca66145bd82592104
devenhukmani <deven.hukmani@gmail.com>
2025-08-28 21:29:00 -0400

    Initial Commit

---
+++ Plant Water App/Plant_Water_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Water_AppApp.swift
+//  Plant Water App
+//
+//  Created by Deven H on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Plant_Water_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plant Water AppTests/Plant_Water_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Water_AppTests.swift
+//  Plant Water AppTests
+//
+//  Created by Deven H on 8/28/25.
+//
+
+import Testing
+@testable import Plant_Water_App
+
+struct Plant_Water_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
